a615123f8ce351514b094a3918c6d510271b2684
Miranda Cascione <miranda.cascione@gmail.com>
2023-03-21 11:18:04 -0700

    add interactivity to input.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -49,33 +49,33 @@ STEP 6:
 import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Counter() {
-  /* STEP 1 */
+
   const [count, setCount] = useState(0);
 
   const increment = () => {
-    /* STEP 4 */
+
     setCount(count + 1);
   };
   const decrement = () => {
-    /* STEP 5 */
+
     setCount(count - 1);
   };
   const reset = () => {
-    /* STEP 6 */
+
     setCount(count - count);
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: count % 2 === 0 ? 'royalBlue' : 'crimson', /* STEP 2 */
+    color: count % 2 === 0 ? 'royalBlue' : 'crimson', 
   };
 
   return (
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number {count} is {count % 2 === 0 ? 'even' : 'odd'} {/* STEP 3 */}
+        Number {count} is {count % 2 === 0 ? 'even' : 'odd'} 
         <div>
         <button id='increment' onClick={increment}>Increment</button>
         <button id='decrement' onClick={decrement}>Decrement</button>
